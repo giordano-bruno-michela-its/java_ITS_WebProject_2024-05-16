4336bdb7e8eb5aa50dcac9f9d0ff91ef3fb3941e
Giordano Bruno Michela <giordano.michela@edu.itspiemonte.it>
2024-05-16 12:46:38 +0200

    its 1

--- src/main/java/model/Dipendente.java
+++ src/main/java/model/Dipendente.java
@@ -1,13 +1,59 @@
 package model;
 
-public class Dipendente {
+public abstract class Dipendente {
 
     private String nome;
 	private String ruolo;
 	private int matricola;
     private static int contatore = 1;
+    private double stipendio;
 
     public Dipendente(String nome) {
         this.nome = nome;
+        this.matricola = contatore++;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRuolo() {
+        return ruolo;
+    }
+
+    public void setRuolo(String ruolo) {
+        this.ruolo = ruolo;
+    }
+
+    public double getStipendio() {
+        return stipendio;
+    }
+
+    public void setStipendio(double stipendio) {
+        this.stipendio = stipendio;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(int matricola) {
+        this.matricola = matricola;
+    }
+
+    @Override
+    public String toString() {
+        return "Dipendente [nome=" + nome +
+                ", ruolo=" + ruolo +
+                ", matricola=" + matricola +
+                ", stipendio=" + stipendio
+				+ "]";
+    }
+
+    public abstract void calcolaStipendio();
+
 }
---
+++ src/main/java/model/Fattorino.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Fattorino extends Dipendente {
+
+	private int numConsegne;
+	private double pagaBase = 5;
+
+    public Fattorino() {
+        super(nome);
+        setRuolo("Fattorino");
+    }
+
+    public void calcolaStipendio() {
+        setStipendio(numConsegne * pagaBase);
+    }
+
+}
